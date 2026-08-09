131f3b72cffa0e99dfa1bb3ea4b862c6a9bddb6a
pacew-webhook <alamremix@gmail.com>
2026-08-09 20:16:56 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.manifestcargo"
-        minSdk = 24
+        minSdk = 26 // <-- Diubah dari 24 ke 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
